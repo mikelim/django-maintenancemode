dd6cb2975b0a98d77e7082acc2bd71df4c6a180e
remco.wendt <remco.wendt@a2cce094-7451-0410-92cc-adf6c7b68777>
2008-07-21 10:18:54 +0000

    Improved setup.py and standard project docs
    git-svn-id: http://django-maintenancemode.googlecode.com/svn/trunk@8 a2cce094-7451-0410-92cc-adf6c7b68777

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
-from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+version = '0.9'
 
 setup(
     name='django-maintenancemode',
-    version='0.9',
-    description='Django maintenance mode is a middleware that allows you to temporary shutdown your site for non staff users and show them a page that the site is down for maintenance. Logged in users having staff credentials can still fully use the site.',
+    version=version,
+    description='Django-maintenancemode allows you to temporary shutdown your site for maintenance work',
+    long_description='Django maintenance mode is a middleware that allows you to temporary shutdown your site for non staff users and show them a page that the site is down for maintenance. Logged in users having staff credentials can still fully use the site.',
     author='Remco Wendt',
     author_email='remco@maykinmedia.nl',
+    license = "BSD",
+    platforms = ["any"],
     url='http://code.google.com/p/django-maintenancemode/',
-    packages=[
-        'maintenancemode',
-        'maintenancemode.conf',
-        'maintenancemode.conf.urls',
-        'maintenancemode.views',
-    ],
+    download_url='',
+    package_dir = {'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
     classifiers=[
             'Development Status :: 4 - Beta',
             'Environment :: Web Environment',
+            'Framework :: Django',
             'Intended Audience :: Developers',
-            'License :: OSI Approved :: New BSD License',
+            'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Topic :: Utilities',
-    ]
+    ],
+    zip_safe=False,
 )
