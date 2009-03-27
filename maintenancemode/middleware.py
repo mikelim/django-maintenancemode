a5df2e5449171833786a050976e4a020d0b2a5be
Toby White <toby@scrtplns.com>
2009-03-27 12:26:27 +0000

    Fix it so we don't try to talk to the db in maintenance mode (or indeed, check INTERAL_IPS)

--- maintenancemode/middleware.py
+++ maintenancemode/middleware.py
@@ -14,15 +14,6 @@ class MaintenanceModeMiddleware(object):
         if not MAINTENANCE_MODE or not cache.get('MAINTENANCE_MODE'):
             return None
 
-        # Allow access if remote ip is in INTERNAL_IPS
-        if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
-            return None
-        
-        # Allow acess if the user doing the request is logged in and a
-        # staff member.
-        if hasattr(request, 'user') and request.user.is_staff:
-            return None
-        
         # Otherwise show the user the 503 page
         resolver = urlresolvers.get_resolver(None)
         
